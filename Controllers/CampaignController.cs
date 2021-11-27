f43daa93020aaada322b94dbf93d15b61901ef03
Unique Jain <jainunique1997@gmail.com>
2021-11-27 20:20:37 +0530

    ok

--- Controllers/CampaignController.cs
+++ Controllers/CampaignController.cs
@@ -48,28 +48,5 @@ namespace Assignment.Controllers
             string json = Newtonsoft.Json.JsonConvert.SerializeObject(campaigns);
             return json;
         }
-
-        public string GetListOfActiveCampaigns()
-        {            
-            WebRequest req = WebRequest.Create(@"https://testapi.donatekart.com/api/campaign");
-            req.Method = "GET";
-            HttpWebResponse resp = req.GetResponse() as HttpWebResponse;
-            DataTable dt = new DataTable();
-            if (resp.StatusCode == HttpStatusCode.OK)
-            {
-                StreamReader rd = new StreamReader(resp.GetResponseStream());
-                string strXml = rd.ReadToEnd();
-                StringReader reader = new StringReader(strXml);
-                dt = Newtonsoft.Json.JsonConvert.DeserializeObject<DataTable>(strXml);
-                DateTime dtToday = DateTime.Now;
-                DateTime dtMonth = dtToday.AddDays(-30);
-                string filter = $"endDate >= '{dtToday}' AND created <= '{dtToday}' AND created > '{dtMonth}'";
-                DataRow[] filteredrows = dt.Select(filter);
-                dt = filteredrows.CopyToDataTable();
-            }
-            string json = Newtonsoft.Json.JsonConvert.SerializeObject(dt);
-
-            return json;
-        }
     }
 }
